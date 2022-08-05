15e8def2544904b8934beacd2958d0e9836e26f0
Zain1306 <zulabdin795@gmail.com>
2022-08-05 17:12:03 +0530

    get.app

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var myApp = require('./myApp');
 var express = require('express');
 var app = express();
 app.get("/",function(req,res){
-  res.send("Hello Express");
+  res.send('Hello Express');
 });
 if (!process.env.DISABLE_XORIGIN) {
   app.use(function(req, res, next) {
