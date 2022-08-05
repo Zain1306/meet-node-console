0cc8ba1d7d9354df1f51711609bc23ed3a912436
Zain1306 <zulabdin795@gmail.com>
2022-08-05 16:09:43 +0530

    get.app

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 app.get("/",function(req,res){
-    req.setEncoding("Hello Express");
+    res.setEncoding("Hello Express");
   });
   
 
