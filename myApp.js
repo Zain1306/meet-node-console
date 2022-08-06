6cf62416d35e46500048a5cf272da227adc3fad0
Zain1306 <zulabdin795@gmail.com>
2022-08-06 20:29:36 +0530

    new

--- myApp.js
+++ myApp.js
@@ -12,20 +12,6 @@ app.use(function(req,res,next){
 // Assets at the /public route
  app.use("/public", express.static(__dirname + "/public"));
 
- var delayInMilliseconds = 1000; //1 second
-
- app.get('/now', (req, res, next) => {
-      req.time = new Date().toString();
-      next()
- }, (req, res) => {
- setTimeout(function() {
-   //your code to be executed after 1 second
-     res.json({
-       time: req.time
-     })
- }, [delayInMilliseconds]);
- }); 
- 
 app.get("/json",function(req,res){
     if(process.env.MESSAGE_STYLE === "uppercase")
     {
@@ -36,8 +22,23 @@ app.get("/json",function(req,res){
     }
 });
 
+function gettime()
+{
+    return new Date().toString();
+}
 
-
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.send({
+      time: req.time
+    });
+  }
+);
 
 
 
