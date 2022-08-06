9cea0779e11555ce3f8c3271432bebf271041e3b
Zain1306 <zulabdin795@gmail.com>
2022-08-06 17:38:43 +0530

    new

--- server.js
+++ server.js
@@ -22,15 +22,10 @@ if (!process.env.DISABLE_XORIGIN) {
   });
 }
  
-app.get("/", (req, res) => {
-  res.send("Hello Express");
-});
-app.get("/",function(req,res){
-   res.sendFile(__dirname+"/views/index.html");
-});
 
-app.use(express.static(__dirname + "/public"));
-app.use("/public",express.static(__dirname + "/public"));
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
