ebada1749097336f46867b3cd559347f107aa97c
Zain1306 <zulabdin795@gmail.com>
2022-08-06 15:31:35 +0530

    my first

--- server.js
+++ server.js
@@ -8,11 +8,9 @@ var myApp = require('./myApp');
 var express = require('express');
 var app = express();
 app.get("/",(req, res) => {
-  res.send("Hello Express");
+  res.send('Hello Express');
 });
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/views/index.html");
-});
+
 if (!process.env.DISABLE_XORIGIN) {
   app.use(function(req, res, next) {
     var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
