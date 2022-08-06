48a50ed36ee0f36d242adbe619f000c5eba1a432
Zain1306 <zulabdin795@gmail.com>
2022-08-06 17:35:29 +0530

    new

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 let express = require('express');
 let app = express();
 
- app.use("/public",express.static(__dirname + "/public"));
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+  });
 
  
  app.use(express.static(__dirname + "/public"));
