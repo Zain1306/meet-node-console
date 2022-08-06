c452752d89091684617d23e8afd70875e817c749
Zain1306 <zulabdin795@gmail.com>
2022-08-06 17:52:35 +0530

    new

--- myApp.js
+++ myApp.js
@@ -1,16 +1,18 @@
 let express = require('express');
 let app = express();
 
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/views/index.html");
-  });
+
 
  
  app.use(express.static(__dirname + "/public"));
 // Assets at the /public route
  app.use("/public", express.static(__dirname + "/public"));
 
-
+app.get("/json",function(req,res){
+    res.json(
+        { "message": "Hello json" }
+    )
+});
 
 
 
